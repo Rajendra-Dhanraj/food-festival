f2443717a63a41ac0b572236cbcd512e0a0d84ef
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-04-15 21:42:14 -0400

    optimization of code and images completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,51 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./assets/js/script.js", // oot of the bundle and the beginning of the dependency graph, so give it the relative path to the client's code
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
+
   output: {
-    path: path.resolve(__dirname, "dist"), // put your bundled code into a folder named dist, which is short for distribution
-    filename: "main.bundle.js",
+    // path: path.resolve(__dirname, "dist"), // put your bundled code into a folder named dist, which is short for distribution
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name(file) {
+                return "[path][name].[ext]"; // rename img file
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/"); // change path
+              },
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder.
     }),
   ],
   mode: "development", // By default, webpack wants to run in production mode. In this mode, webpack will minify our code for us automatically. We want our code to run in development mode
